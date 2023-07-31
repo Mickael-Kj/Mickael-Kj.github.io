9853463071ae22be8887877e6329c07eaff704f6
Your Name <jesaispas@github.com>
2023-07-31 22:41:25 +0200

    Modal video

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,31 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
+
+// MODAL SKATE VIDEO
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+let skate = document.getElementById("skate");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+skate.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
